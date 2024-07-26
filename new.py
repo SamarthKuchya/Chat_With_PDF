30879829f072e537ea05be08b3a5deb737e7fdb4
SamarthKuchya <129831011+SamarthKuchya@users.noreply.github.com>
2024-07-26 17:58:12 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,78 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GENAI_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks
+
+def get_conversational_chain():
+    prompttemplate='''Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    '''
+    model=ChatGoogleGenerativeAI(model='gemini-pro',temperature=0.3)
+
+    prompt=PromptTemplate(template=prompttemplate,input_variables=['context','question'])
+    chain=load_qa_chain(llm=model,chain_type='stuff',prompt=prompt)
+    return chain
+
+def user_input(user_question,text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+    vector_store = FAISS.from_texts(text_chunks,embedding=embeddings)
+    # new_db=FAISS.load_local('tmp/',embeddings=embeddings,allow_dangerous_deserialization=True)
+    docs=vector_store.similarity_search(user_question)
+
+    chain=get_conversational_chain()
+
+    response=chain(
+        {'input_documents':docs,'question':user_question}
+        ,return_only_outputs=True)
+    
+    print(response)
+    st.write('Reply: ',response['output_text'])
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF")
+    user_question = st.text_input("Ask a Question from the PDF Files")
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files ( Which Are Readable By Computers ) and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                st.session_state.text_chunks = get_text_chunks(raw_text)
+                st.success("Done")
+                
+    
+    if user_question:
+        print(st.session_state.text_chunks)
+        user_input(user_question,st.session_state.text_chunks)
+
+if __name__ == "__main__":
+    main()
